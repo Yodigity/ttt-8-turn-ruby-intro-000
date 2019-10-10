a0d89b0a912865067f4d17a8e4a21c6b42cfd431
Aaron Gokani <aarongokani@hotmail.com>
2019-10-10 03:03:47 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,34 @@
+
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+
+
+def input_to_index(input)
+  pos = input -= 1
+end
+
+def position_taken?(board, place)
+  if board[place] == " " || board[place] == "" || board[place] == nil
+    return true
+  else 
+    return false
+  end
+end
+
+def valid_move?(pos)
+  if pos >=0 && pos <= 8 && position_taken?(board, )
+    return true
+  else 
+    return false
+  end
+end
+  
+  
